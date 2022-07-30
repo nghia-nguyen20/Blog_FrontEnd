71330118df7c23f543ac8694aea41f70d0fffb4f
nghianguyen <nghia.nguyen.20220@gmail.com>
2022-07-30 11:12:37 +0700

    initiall commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import Home from './components/Home';
-import Contact from './components/Contact';
-import Product from './components/Product';
-import News from './components/News';
+import Header from "./components/Header";
+import Pattern from "./components/Pattern";
+import Pricing from "./components/Pricing";
+import Support from "./components/Support";
+import UltrafitShop from "./components/Ultrafit_shop";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/contact">
-            <Contact phone="01223445" name="tui" address="lam dong"/>
-          </Route>
-          <Route path="/product">
-            <Product description="sp dep" />
-          </Route>
-          <Route path="/news">
-            <News />
-          </Route>
-        </Switch>
+        <Route exact path="/Pattern">
+          <Pattern />
+        </Route>
+        <Route path="/">
+          <Pricing />
+        </Route>
+        <Route path="/Support">
+          <Support />
+        </Route>
+        <Route path="/Ultrafit_shop">
+          <UltrafitShop />
+        </Route>
       </div>
     </Router>
   );
